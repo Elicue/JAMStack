78085f84e5b48587363404eb76e5d42938fdda88
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2024-02-01 19:27:49 +0100

    :fire: Restart

--- packages/app/src/pages/index.vue
+++ packages/app/src/pages/index.vue
@@ -1,4 +1,15 @@
 <script lang="ts" setup>
+// import { useAsyncData } from "../../.nuxt/imports";
+
+const { find } = useStrapi();
+
+const { data: recipes, pending, error } = await useAsyncData('recipes', () => $fetch('http://localhost:1337/api/recipes?populate=*'));
+console.log('recipes', recipes);
+
+onMounted(() => {
+  console.log(recipes, pending, error);
+});
+
 </script>
 
 <template>
@@ -13,6 +24,15 @@
           Exemple de recherche
         </NuxtLink>
       </div>
+    </div>
+
+    <div v-if="recipes">
+      <ul class="grid grid-cols-4">
+        <div v-for="recipe in recipes.data" :key="recipe.id" class="border-solid border-2 border-black w-fit p-6 m-y-3">
+          <li class="list-none"> {{ recipe.title }} </li>
+          <li class="list-none"> {{ recipe.description }} </li>
+          </div>
+      </ul>
     </div>
   </div>
 </template>
--- packages/strapi/types/generated/contentTypes.d.ts
+++ packages/strapi/types/generated/contentTypes.d.ts
@@ -362,6 +362,39 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiRecipeRecipe extends Schema.CollectionType {
+  collectionName: 'recipes';
+  info: {
+    singularName: 'recipe';
+    pluralName: 'recipes';
+    displayName: 'Recipes';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    description: Attribute.RichText;
+    slug: Attribute.UID & Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::recipe.recipe',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::recipe.recipe',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -778,6 +811,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::recipe.recipe': ApiRecipeRecipe;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
