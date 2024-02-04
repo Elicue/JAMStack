274ae68dcf03fb930b5bdfa56c485ec7f39f217e
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2024-02-04 18:27:23 +0100

    :wastebasket: Clear Code

--- packages/app/src/pages/index.vue
+++ packages/app/src/pages/index.vue
@@ -1,18 +1,12 @@
 <script lang="ts" setup>
 import type { ITag } from '~/models/recipes.model';
-// import { useAsyncData } from "../../.nuxt/imports";
 
 const { find } = useStrapi();
 const search = useSearchStore();
 
-// const { data: recipes, pending, error } = await useAsyncData('recipes', () => $fetch('http://localhost:1337/api/recipes?populate=*'));
-// console.log('recipes', recipes);
 
 const { data: recipes } = await find('recipes', { populate: '*' });
 console.log('recipes', recipes);
-
-// const { data: tags } = await find('tags', { populate: '*' });
-// console.log('tags', tags);
 
 const { data: tags } = useAsyncData(
   'tags',
@@ -33,18 +27,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="bg-[#f3f1f2] min-h-screen relative w-full overflow-x-hidden p-4 flex flex-col" v-if="!search.pending && search.sortedByTags">
-    <div class="flex flex-col items-center gap-y-4">
-      <!-- <h1>Démarrage Nuxt Strapi</h1> -->
-      <div class="gap-x-4 hidden">
-        <NuxtLink to="/start">
-          Documentation
-        </NuxtLink>
-        <NuxtLink to="/exemple-recherche">
-          Exemple de recherche
-        </NuxtLink>
-      </div>
-    </div>
+  <div class="bg-[#f3f1f2] min-h-screen relative w-full overflow-x-hidden p-4 flex flex-col">
 
     <nav class="flex flex-row justify-between w-full absolute top-0 p-6 p-r-16 p-y-12 z-100 items-center mx-auto">
       <img src="../assets/logo.png" alt="logo" class="logo w-[6vw]">
@@ -67,59 +50,60 @@ onMounted(() => {
         </div>
       </div>
     </header>
-
-    <div v-if="search.sortedByTags" id="recipes" class="z-10 flex flex-col w-full">
-      <div v-if="tags">
-      <div class="search flex flex-row justify-between items-center ">
-        <h2 class="list-none uppercase text-[2.5rem] p-y-3 tracking-wider">All our recipes</h2>
-        <div
-          class="p-4 p-x-6 p-r-40 border-none rounded-full border-[#141414] border-solid border-[3px] items-center flex flex-row gap-2 w-3/12">
-          <img src="../assets/search.svg" alt="search">
-          <input id="search" v-model="search.query"
-            class="border-none text-[#141414] outline-none text-[1rem] bg-transparent placeholder-[#141414] w-full"
-            placeholder="Searching for something ?" type="search">
-        </div>
-      </div>
-        <div class="tags flex flex-row gap-4 items-center">
-          <ul class=" flex flex-row gap-4 p-y-4">
-            <div v-for="tag in tags?.data" :key="tag.id">
-              <li :class="[search.queryTags.includes(tag.slug) ? 'bg-[#f6c944] p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer' : 'bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer']" @click="addTag(tag.slug)">
-                {{ tag.title }}</li>
-              <!-- <Tag/> -->
-            </div>
-          </ul>
-          <li class="bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer"  @click="search.resetTags">Reset selected tags</li>
-        </div>
-      </div>
-      <ul class="grid grid-cols-4 gap-6 gap-y-1 w-full p-0">
-        <div v-for="recipe in search.sortedByTags" :key="recipe.id"
-          class="w-fill p-6 m-y-3  gap-y-2 flex flex-col rounded-xl bg-white justify-between min-h-[62vh]">
-          <div class="w-fill flex flex-col">
-            <img :src="recipe.image[0].url" :alt="recipe.image[0].name"
-              class="w-fill h-80 min-h-80 object-cover object-bottom rounded-lg">
-            <li @click="$router.push(`/recipes/${recipe.slug}`)"
-              class="list-none text-xl bg-[#f6c944] p-x-5 p-y-1 tracking-wider m-y-3 rounded-lg">
-              {{ recipe.title }}</li>
-            <!-- <li class="list-none"> {{ recipe.tags[0] }} </li> -->
-            <li class="list-none tracking-wide leading-6"> {{ recipe.description }} </li>
+    <client-only>
+      <div v-if="search.sortedByTags" id="recipes" class="z-10 flex flex-col w-full">
+        <div v-if="tags">
+        <div class="search flex flex-row justify-between items-center ">
+          <h2 class="list-none uppercase text-[2.5rem] p-y-3 tracking-wider">All our recipes</h2>
+          <div
+            class="p-4 p-x-6 p-r-40 border-none rounded-full border-[#141414] border-solid border-[3px] items-center flex flex-row gap-2 w-3/12">
+            <img src="../assets/search.svg" alt="search">
+            <input id="search" v-model="search.query"
+              class="border-none text-[#141414] outline-none text-[1rem] bg-transparent placeholder-[#141414] w-full"
+              placeholder="Searching for something ?" type="search">
           </div>
-          <div>
-            <ul class="flex flex-row gap-4">
-              <li
-                v-for="(tag, index) in recipe.tags"
-                :key="index"
-                class="bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer"
-              >
-                {{ tag.title }}
-              </li>
+        </div>
+          <div class="tags flex flex-row gap-4 items-center">
+            <ul class=" flex flex-row gap-4 p-y-4">
+              <div v-for="tag in tags?.data" :key="tag.id">
+                <li :class="[search.queryTags.includes(tag.slug) ? 'bg-[#f6c944] p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer' : 'bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer']" @click="addTag(tag.slug)">
+                  {{ tag.title }}</li>
+                <!-- <Tag/> -->
+              </div>
             </ul>
-            <li @click="$router.push(`/recipes/${recipe.slug}`)"
-              class="border-solid border-2 border-[#141414] list-none p-y-3 bg-white tracking-wider m-y-3 hover:cursor-pointer hover:scale-105 transition-all text-center rounded-full">
-              See more</li>
+            <li class="bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer"  @click="search.resetTags">Reset selected tags</li>
           </div>
         </div>
-      </ul>
-    </div>
+        <ul class="grid grid-cols-4 gap-6 gap-y-1 w-full p-0">
+          <div v-for="recipe in search.sortedByTags" :key="recipe.id"
+            class="w-fill p-6 m-y-3  gap-y-2 flex flex-col rounded-xl bg-white justify-between min-h-[62vh]">
+            <div class="w-fill flex flex-col">
+              <img :src="recipe.image[0].url" :alt="recipe.image[0].name"
+                class="w-fill h-80 min-h-80 object-cover object-bottom rounded-lg">
+              <li @click="$router.push(`/recipes/${recipe.slug}`)"
+                class="list-none text-xl bg-[#f6c944] p-x-5 p-y-1 tracking-wider m-y-3 rounded-lg">
+                {{ recipe.title }}</li>
+              <!-- <li class="list-none"> {{ recipe.tags[0] }} </li> -->
+              <li class="list-none tracking-wide leading-6"> {{ recipe.description }} </li>
+            </div>
+            <div>
+              <ul class="flex flex-row gap-4">
+                <li
+                  v-for="(tag, index) in recipe.tags"
+                  :key="index"
+                  class="bg-[#f6c944] bg-opacity-50 p-4 p-x-6 w-fit capitalize rounded-full list-none hover:cursor-pointer"
+                >
+                  {{ tag.title }}
+                </li>
+              </ul>
+              <li @click="$router.push(`/recipes/${recipe.slug}`)"
+                class="border-solid border-2 border-[#141414] list-none p-y-3 bg-white tracking-wider m-y-3 hover:cursor-pointer hover:scale-105 transition-all text-center rounded-full">
+                See more</li>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </client-only>
   </div>
 </template>
 
